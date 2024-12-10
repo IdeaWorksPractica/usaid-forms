56e08aa6225fcbdd40262f3bf8f8148f8408289c
HaroruDa3 <harol.kock34@gmail.com>
2024-12-10 14:06:53 -0600

    cambios para redireccion de rutas manejadas por react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
+  publicDir: 'public', // Carpeta para archivos estáticos
+});
